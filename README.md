5d634557b1df9a55266e0a853e7e4ef6ab244c57
Jihyo Kim <njsh4261@gmail.com>
2022-11-01 14:44:05 +0900

    fix: replace ProcessPoolExecutor with multiprocessing.Process to handle timeout
    - edit README.md

--- README.md
+++ README.md
@@ -3,8 +3,14 @@
 - Python 3.10.8 사용
 - FastAPI 서버에서 웹소켓 + 멀티프로세싱
 - 웹소켓을 통해 서버에 작업 명령 할당, 클라이언트에 작업 상태 전달
-- ProcessPoolExecutor를 통한 멀티프로세싱 처리
+- Timeout 기능 추가
 
 ## 실행
-- 서버 실행: `uvicorn main:app`
+- 서버 실행
+    ```
+    > python -m venv venv
+    > source venv/bin/activate
+    (venv) > pip install -r requirements.txt
+    (venv) > uvicorn main:app
+    ```
 - `client/task_manager.html` 통해 접속
--- server/src/task/task_manager.py
+++ server/src/task/task_manager.py
@@ -1,24 +1,31 @@
 import asyncio
 import time
-from os import cpu_count
-from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import Process
 from src.ws.connection_manager import ConnectionManager
 from src.matrix.matrix import matrix_multiplication
 
 
+def run_task(func, timeout, *args, **kwargs):
+    process = Process(target=func, args=args, kwargs=kwargs)
+    process.start()
+    process.join(timeout)
+    if process.is_alive():
+        process.terminate()
+        raise asyncio.exceptions.TimeoutError()
+
+
 class TaskManager:
-    def __init__(self, connection_manager, loop, max_workers=cpu_count()):
+    def __init__(self, connection_manager, loop):
         self.connection_manager: ConnectionManager = connection_manager
         self.loop: asyncio.AbstractEventLoop = loop
-        self.executor = ProcessPoolExecutor(max_workers)
 
     async def add_task(self, size, task_id, start_time, timeout: float = 30.0):
         try:
             result = await asyncio.wait_for(
                 self.loop.run_in_executor(
-                    self.executor, matrix_multiplication, size
+                    None, run_task, matrix_multiplication, timeout, size
                 ),
-                timeout
+                None
             )
             await self.connection_manager.broadcast({
                 "id": task_id,
